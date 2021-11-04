5ad0d64aa2d633d66870f2e11d364771a3bf776b
benjo27 <91257741+benjo27@users.noreply.github.com>
2021-11-04 17:24:43 +0100

    Modification du while + indentation-> parfaite

--- PROJET_PIRATE/src/main/Main.java
+++ PROJET_PIRATE/src/main/Main.java
@@ -17,84 +17,85 @@ public class Main {
 		String listobj = "";
 		Graphe graphe = new Graphe();
 
-		System.out.println("Combien de pirate possede votre equipage ?");
-		n = sc.nextInt();
-		
-		while (n>0 && n<27) {
-				char c = 'A';
-				for (int i=0; i<n; i++) {
-					String s = String.valueOf(c);
-					graphe.ajoutNoeud(s);
-					listobj=listobj+(i+1)+" ";
-					c++;
-				}
-
-			do {
-				System.out.println("***** MENU *****");
-				System.out.println("1- Ajouter une relation");
-				System.out.println("2- Ajouter des préférences");
-				System.out.println("3- Voir les preferences");
-				System.out.println("4- Fin");
-				choix = sc.nextInt();
-
-				switch(choix) {
-					case 1 : System.out.println("Choisir les pirates qui ne s'aiment pas :");
-					System.out.println("Nom du premier pirate ? ");
-					e1 = sc.next();
-					System.out.println("Nom du deuxieme pirate ? ");
-					e2 = sc.next();
-					graphe.ajoutRelation(e1,e2);
-					break;
-
-					case 2 : System.out.println("Nom du pirate suivi de ses préférence :");
-					sc.nextLine();
-					pref = sc.nextLine();
-					graphe.ajoutPreference(pref);
-					break;
-
-					case 3 : System.out.println("Voici la liste d'adjacence des pirates ainsi que de leur préférences:");
-					System.out.println(graphe.toString());
-					break;
-				}
-
-				if (choix>4) {
-					System.out.println("Erreur, votre choix ne figure pas dans le menu");
-				}
-			}while(choix !=4);
-
-			Affectation affectation = new Affectation(graphe, listobj);
-			System.out.println(affectation.toString());
-			String o1, o2;
-
-			do {
-				System.out.println("***** MENU numero 2 *****");
-
-				System.out.println("1- Echanger objet");
-				System.out.println("2- Afficher coût");
-				System.out.println("3- Fin");
-				option= sc.nextInt();
-
-				switch(option) {
-					case 1 : System.out.println("Echanger les objets : ");
-					System.out.println("A quel pirate appartient l'objet que vous voulez changer ? ");
-					o1 = sc.next();
-					System.out.println("Choisir le deuxieme pirate ");
-					o2 = sc.next();
-					affectation.echanger(o1,o2);
-					System.out.println(affectation.toString());
-					break;
-
-					case 2 : System.out.println("Le cout est équivalent à : " +affectation.calculCout(graphe));
-					break;
-				}
-				
-				if (option>3) {
-					System.out.println("Erreur, votre choix ne figure pas dans le menu");
-				}
-			}while(option !=3);
-			
-			System.out.println(affectation.toString());
+		while (!(n>0 && n<27)) {
+			System.out.println("Combien de pirate possede votre equipage ?");
+			n = sc.nextInt();
 		}
+
+		char c = 'A';
+		for (int i=0; i<n; i++) {
+			String s = String.valueOf(c);
+			graphe.ajoutNoeud(s);
+			listobj=listobj+(i+1)+" ";
+			c++;
+		}
+
+		do {
+			System.out.println("***** MENU *****");
+			System.out.println("1- Ajouter une relation");
+			System.out.println("2- Ajouter des préférences");
+			System.out.println("3- Voir les preferences des pirates");
+			System.out.println("4- Fin");
+			choix = sc.nextInt();
+
+			switch(choix) {
+				case 1 : System.out.println("Choisir les pirates qui ne s'aiment pas :");
+				System.out.println("Nom du premier pirate ? ");
+				e1 = sc.next();
+				System.out.println("Nom du deuxieme pirate ? ");
+				e2 = sc.next();
+				graphe.ajoutRelation(e1,e2);
+				break;
+
+				case 2 : System.out.println("Nom du pirate suivi de ses préférence :");
+				sc.nextLine();
+				pref = sc.nextLine();
+				graphe.ajoutPreference(pref);
+				break;
+
+				case 3 : System.out.println("Voici la liste d'adjacence des pirates ainsi que de leur préférences:");
+				System.out.println(graphe.toString());
+				break;
+			}
+
+			if (choix>4) {
+				System.out.println("Erreur, votre choix ne figure pas dans le menu");
+			}
+		}while(choix !=4);
+
+		Affectation affectation = new Affectation(graphe, listobj);
+		System.out.println(affectation.toString());
+		String o1, o2;
+
+		do {
+			System.out.println("***** MENU numero 2 *****");
+
+			System.out.println("1- Echanger objet");
+			System.out.println("2- Afficher coût");
+			System.out.println("3- Fin");
+			option= sc.nextInt();
+
+			switch(option) {
+				case 1 : System.out.println("Echanger les objets : ");
+				System.out.println("A quel pirate appartient l'objet que vous voulez changer ? ");
+				o1 = sc.next();
+				System.out.println("Choisir le deuxieme pirate ");
+				o2 = sc.next();
+				affectation.echanger(o1,o2);
+				System.out.println(affectation.toString());
+				break;
+
+				case 2 : System.out.println("Le cout est équivalent à : " +affectation.calculCout(graphe));
+				break;
+			}
+
+			if (option>3) {
+				System.out.println("Erreur, votre choix ne figure pas dans le menu");
+			}
+		}while(option !=3);
+
+		System.out.println(affectation.toString());
+
 		sc.close();
 	}
 }
