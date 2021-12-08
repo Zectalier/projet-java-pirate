0a0f54083aa348a4dc822f2f0fcd0e05df8e7511
Zectalier <hutony2001@gmail.com>
2021-12-08 08:57:40 +0100

    Class' name changed

--- PROJET_PIRATE/doc/type-search-index.js
+++ PROJET_PIRATE/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"main","l":"Affectation"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"main","l":"Graphe"},{"p":"main","l":"GrapheTest"},{"p":"main","l":"Main"},{"p":"main","l":"Noeud"}];updateSearchResults();
+typeSearchIndex = [{"p":"main","l":"Affectation"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"main","l":"Equipage"},{"p":"main","l":"GrapheTest"},{"p":"main","l":"Main"},{"p":"main","l":"Pirate"}];updateSearchResults();
