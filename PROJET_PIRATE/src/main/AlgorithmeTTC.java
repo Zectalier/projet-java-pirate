bd05bbe825d8f3bbcfc0f06a766b0dd9878bb861
Zectalier <hutony2001@gmail.com>
2021-12-15 15:46:10 +0100

    Tentative de passer par algo ttc

---
+++ PROJET_PIRATE/src/main/AlgorithmeTTC.java
@@ -0,0 +1,147 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/***
+ * Classe qui essaye de résoudre le problème avec l'algorithme Top Trading Cycle, 
+ * cependant ne marche pas pour trouver une solution optimale dans notre problème du à la jalousie possible entre pirates.
+ * En effet l'algorithme essaye de trouver une solution le plus équitable possible, mais prends pas en compte d'éventuel problèmes de jalousie 
+ * entre plusieurs individus
+ * @author Hu Tony
+ *
+ */
+public class AlgorithmeTTC {
+
+	private Map<Pirate, String> affectation;
+	
+	public AlgorithmeTTC(Affectation affectation) {
+		//Affectation affectation = new Affectation(graphe,listeobjet);
+		Map<Pirate, String> topPrefTout = new HashMap<Pirate, String>();
+		Map<Pirate,ArrayList<String>> listePirate = new HashMap<Pirate,ArrayList<String>>();
+		boolean end = false;
+		for(Pirate p : affectation.getAffectation().keySet()) {
+			listePirate.put(p, (new ArrayList<String>()));
+			listePirate.get(p).add(affectation.getAffectation().get(p));
+			listePirate.get(p).add("0");
+		}
+		for(Pirate p : affectation.getAffectation().keySet()) {
+			topPrefTout.put(p, topPreference(p,listePirate,affectation));
+			if(topPrefTout.get(p).equals(affectation.getAffectation().get(p)) && listePirate.get(p).get(1).equals("0")) {
+				listePirate.get(p).set(1,"1");
+			}
+		}
+		while(!end) {
+			end = true;
+			for(Pirate p : affectation.getAffectation().keySet()) {
+				topPrefTout.put(p, topPreference(p,listePirate,affectation));
+			}
+			for(Pirate p : listePirate.keySet()) {
+				topPrefTout.put(p, topPreference(p,listePirate,affectation));
+				if(!listePirate.get(p).get(1).equals("1")) {
+					if(topPrefTout.get(p).equals(affectation.getAffectation().get(p)) && listePirate.get(p).get(1).equals("0")) {
+						listePirate.get(p).set(1,"1");
+					}
+					else {
+						for(Pirate v : listePirate.keySet()) {
+							if(topPrefTout.get(p).equals(listePirate.get(v).get(0)) && listePirate.get(v).get(1).equals("0")) {
+								listePirate.get(v).set(0, listePirate.get(p).get(0));
+								listePirate.get(p).set(0, topPrefTout.get(p));
+								affectation.echanger(p.getEtiquette(), v.getEtiquette());
+								listePirate.get(p).set(1,"1");
+								break;
+							}
+						}
+					}
+					end = false;
+				}
+			}
+		}
+		this.affectation = affectation.getAffectation();
+	}
+	
+	private String topPreference(Pirate pirate, Map<Pirate,ArrayList<String>>listePirate, Affectation affectation) {
+		int i = 0;
+		boolean found;
+		String topPref = "";
+		while(i != -1) {
+			found = false;
+			topPref = pirate.getPreference().get(i);
+			if(topPref.equals(listePirate.get(pirate).get(0))) {
+				return topPref;
+			}
+			else{
+				while(!found) {
+					for(Pirate p : listePirate.keySet()) {
+						if(!listePirate.get(p).get(1).equals("1") && !p.equals(pirate) && listePirate.get(p).get(0).equals(topPref)) {
+							found = true;
+							break;
+						}
+					}
+					if(found == true) {
+						i=-1;
+					}
+					else{
+						found = true;
+						i++;
+					}
+				}
+			}
+		}
+		return topPref;
+	}
+	
+	public int calculCout(Equipage graphe) {
+		int cout = 0;
+		int i;
+		boolean found;
+		String affecte,affecteVoisin;
+		int affectePosition, affectePositionVoisin;
+
+		for (Pirate v : graphe.getMap().keySet()) {
+			found = false;
+			i = 0;
+			affecte = affectation.get(v);
+			affectePosition = v.getPreference().indexOf(affecte);
+
+			//Cas si trésor affecté est le favori du pirate
+			if(i == 0) {
+				if(affecte.equals(v.getPreference().get(0))) {
+					found = true;
+				}
+			}
+
+			while(!found) {
+
+				//On check pour chacun des voisins du pirate
+				for(Pirate u : graphe.getMap().get(v)) {
+					affecteVoisin = affectation.get(u);
+					affectePositionVoisin = u.getPreference().indexOf(affecteVoisin);
+
+					//On regarde si l'ordre de préference de l'objet du voisin est inférieur ou égal à son ordre de préférence pour l'objet et que l'ordre de l'objet qu'il a reçu est supérieur à SON ordre de l'objet du voisin
+					if(affectePositionVoisin<=affectePosition && v.getPreference().indexOf(affecteVoisin)<affectePosition) {
+						cout++;
+						break;
+					}
+				}
+				found = true;
+			}
+		}
+
+		return cout;
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+
+		for (Pirate v : affectation.keySet()) {
+			builder.append(v.getEtiquette() + ": ");
+			builder.append(affectation.get(v));
+			builder.append("\n");
+		}
+		return (builder.toString());
+	}
+}
---
+++ PROJET_PIRATE/src/main/AlgorithmeTTCTest.java
@@ -0,0 +1,50 @@
+package main;
+
+public class AlgorithmeTTCTest {
+
+public static void main (String[]args) {
+		
+		Equipage graphe = new Equipage();
+		graphe.ajoutObjet("1");
+		graphe.ajoutObjet("2");
+		graphe.ajoutObjet("3");
+		graphe.ajoutObjet("4");
+		graphe.ajoutObjet("5");
+		graphe.ajoutObjet("6");
+		graphe.ajoutPirate("1");
+		graphe.ajoutPirate("2");
+		graphe.ajoutPirate("3");
+		graphe.ajoutPirate("4");
+		graphe.ajoutPirate("5");
+		graphe.ajoutPirate("6");
+		
+		System.out.println(graphe.ajoutPreference("1 3 2 4 1 5 6"));
+		System.out.println(graphe.ajoutPreference("2 3 5 6 1 2 4"));
+		System.out.println(graphe.ajoutPreference("3 3 1 6 5 4 2"));
+		System.out.println(graphe.ajoutPreference("4 2 5 6 4 3 1"));
+		System.out.println(graphe.ajoutPreference("5 1 3 2 4 5 6"));
+		System.out.println(graphe.ajoutPreference("6 2 4 5 6 1 3"));
+		
+		graphe.ajoutRelation("1", "2");
+		graphe.ajoutRelation("2", "3");
+		graphe.ajoutRelation("3", "4");
+		graphe.ajoutRelation("4", "5");
+		graphe.ajoutRelation("5", "6");
+		graphe.ajoutRelation("1", "6");
+		graphe.ajoutRelation("2", "5");
+		graphe.ajoutRelation("3", "4");
+		graphe.ajoutRelation("1", "3");
+		graphe.ajoutRelation("1", "4");
+		graphe.ajoutRelation("1", "5");
+		Affectation affectation = new Affectation(graphe);
+		affectation.echanger("1", "3");
+		affectation.echanger("4", "2");
+		affectation.echanger("4", "5");
+		System.out.println("Affectation:\n"+ affectation.toString());
+		System.out.println("Cout: "+affectation.calculCout());
+		AlgorithmeTTC affectauto = new AlgorithmeTTC(affectation);
+		System.out.println("Affectation:\n"+ affectauto.toString());
+		System.out.println("Cout: "+affectauto.calculCout(graphe));
+	}
+
+}
